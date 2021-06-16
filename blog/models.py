60536d02d21a0f3ee321a87e1021bbd7579a4450
bzmaxat <b.ma1xat@gmail.com>
2021-06-16 15:17:56 +0600

    first commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    image = models.ImageField(upload_to='yes')
+    height = models.IntegerField()
+    difficulty = models.CharField(max_length=20)
+    route = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
