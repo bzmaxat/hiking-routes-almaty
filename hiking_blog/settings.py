78a43a43be4b393ade890300d204ff04e700d20b
bzmaxat <b.ma1xat@gmail.com>
2021-11-22 17:50:19 +0000

    Remove old stuff

--- hiking_blog/settings.py
+++ hiking_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vu7c+zua=-_m&h@=u6xoul)_1ry9%wo1&b6-37t-4^jr@ndmst'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bzmaxat.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ import os
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
