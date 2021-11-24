9af4f6ab771d5c5a11220e7c43e7fd9abbfc0f43
bzmaxat <b.ma1xat@gmail.com>
2021-11-24 22:21:58 +0600

    Local settings

--- hiking_blog/settings.py
+++ hiking_blog/settings.py
@@ -127,7 +127,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file file. You must be on production.")
